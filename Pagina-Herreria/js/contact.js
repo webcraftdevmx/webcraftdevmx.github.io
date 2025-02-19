941725c2e6965304f89104a4eabea39bfae66d25
DanielRodriguez <dany.15.dars@gmail.com>
2025-02-18 23:55:38 -0800

    Se suben cambios finales sobre pagina de contacto

--- Pagina-Herreria/js/contact.js
+++ Pagina-Herreria/js/contact.js
@@ -1,13 +1,15 @@
 const nombre = document.getElementById("Nombre");
 const email = document.getElementById("email");
 const mensaje = document.getElementById("Mensaje");
-
+const mensajeForm = document.getElementById("mensajeForm");
+const K_MENSAJE_ENV = "¡Tu mensaje ha sido enviado!";
+const K_MENSAJE_ERR = "¡Hubo un error al enviar el mensaje!";
 document
   .getElementById("contactForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
 
-    /* emailjs
+    emailjs
       .send("service_6i9j3yn", "template_gtbqyea", {
         from_name: nombre.value,
         from_email: email.value,
@@ -16,16 +18,14 @@ document
       })
       .then(
         function (response) {
-          console.log("Correo enviado con éxito:", response);
-          alert("¡Tu mensaje ha sido enviado!");
+          if (response.status === 200)
+            createMessage("#25dac5", "success", K_MENSAJE_ENV);
         },
         function (error) {
           console.error("Error al enviar el correo:", error);
-          alert("Ocurrió un error. Intenta nuevamente.");
+          createMessage("#da3125", "error", K_MENSAJE_ERR);
         }
-      );*/
-
-    alert("¡Tu mensaje ha sido enviado!");
+      );
     cleanInputs();
   });
 
@@ -33,4 +33,15 @@ function cleanInputs() {
   nombre.value = "";
   email.value = "";
   mensaje.value = "";
+}
+
+function createMessage(color, evento, mensaje) {
+  mensajeForm.classList.remove("Ocultar");
+  mensajeForm.classList.add(evento);
+  mensajeForm.textContent = mensaje;
+  setTimeout(() => {
+    mensajeForm.classList.add("Ocultar");
+    mensajeForm.classList.remove(evento);
+    mensajeForm.style.backgroundColor = color;
+  }, 3000);
 }
