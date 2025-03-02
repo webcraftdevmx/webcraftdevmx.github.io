cbc37dc142a3ebacce584068b458c523d5a649cf
Selvin Toledo <Selvin3901@gmail.com>
2025-03-01 22:53:37 -0800

    cambios en pagina y logica de filtos mis trabajos

--- Pagina-Herreria/scripts/index.js
+++ Pagina-Herreria/scripts/index.js
@@ -10,3 +10,11 @@ btnAbrir.addEventListener("click", () => {
 btnCerrar.addEventListener("click", () => {
   menu.classList.remove("visible");
 });
+
+function cambiarPagina(Element){
+  console.log("Entre")
+  let PaginaSeleccionada = Element.value;
+  if(PaginaSeleccionada){
+      window.location.href = PaginaSeleccionada;
+  }
+}
--- Pagina-Herreria/scripts/nTrabajo.js
+++ Pagina-Herreria/scripts/nTrabajo.js
@@ -1,6 +1,122 @@
-function cambiarPagina(Element){
-    let PaginaSeleccionada = Element.value;
-    if(PaginaSeleccionada){
-        window.location.href = PaginaSeleccionada;
+const Objetos = document.querySelectorAll('.obj');
+const Portones = document.getElementById('Portones');
+const Puertas = document.getElementById('Puertas');
+const Protecciones = document.getElementById('Protecciones');
+const Botones = document.querySelectorAll('.but');
+
+Portones.addEventListener('click', () => {
+
+    Botones.forEach((element) => {
+        if (element.classList.contains('Active') && element.id != 'Portones'){
+            element.classList.remove('Active');
+        }
+    } )
+
+    Objetos.forEach((element) => {
+        if (element.classList.contains('Oculta')){
+            element.classList.remove('Oculta');
+        }
+    } )
+
+    if (Portones.classList.contains('Active')){
+        Portones.classList.remove('Active');
+    
+        /*Objetos.forEach((element) => {
+            //console.log(element);
+            if (element.classList.contains('Oculta')){
+                element.classList.remove('Oculta');
+                //console.log(element);
+            }
+        } )*/
+
     }
-}
+    else
+    {
+        Portones.classList.add('Active');
+        Objetos.forEach((element) => {
+            //console.log(element);
+            if (!element.classList.contains('portones')){
+                element.classList.add('Oculta');
+                //console.log(element);
+            }
+        } )
+    }
+})
+
+Puertas.addEventListener('click', () => {
+
+    Botones.forEach((element) => {
+        if (element.classList.contains('Active') && element.id != 'Puertas'){
+            element.classList.remove('Active');
+        }
+    } )
+
+    Objetos.forEach((element) => {
+        if (element.classList.contains('Oculta')){
+            element.classList.remove('Oculta');
+        }
+    } )
+
+    if (Puertas.classList.contains('Active')){
+        Puertas.classList.remove('Active');
+    
+        /*Objetos.forEach((element) => {
+            //console.log(element);
+            if (element.classList.contains('Oculta')){
+                element.classList.remove('Oculta');
+                //console.log(element);
+            }
+        } )*/
+
+    }
+    else
+    {
+        Puertas.classList.add('Active');
+        Objetos.forEach((element) => {
+            //console.log(element);
+            if (!element.classList.contains('puertas')){
+                element.classList.add('Oculta');
+                //console.log(element);
+            }
+        } )
+    }
+})
+
+Protecciones.addEventListener('click', () => {
+    Botones.forEach((element) => {
+        if (element.classList.contains('Active') && element.id != 'Protecciones'){
+            element.classList.remove('Active');
+        }
+    } )
+
+    Objetos.forEach((element) => {
+        if (element.classList.contains('Oculta')){
+            element.classList.remove('Oculta');
+        }
+    } )
+
+    if (Protecciones.classList.contains('Active')){
+        Protecciones.classList.remove('Active');
+    
+        /*Objetos.forEach((element) => {
+            //console.log(element);
+            if (element.classList.contains('Oculta')){
+                element.classList.remove('Oculta');
+                //console.log(element);
+            }
+        } )*/
+
+    }
+    else
+    {
+        Protecciones.classList.add('Active');
+        Objetos.forEach((element) => {
+            //console.log(element);
+            if (!element.classList.contains('protecciones')){
+                element.classList.add('Oculta');
+                //console.log(element);
+            }
+        } )
+    }
+})
+
